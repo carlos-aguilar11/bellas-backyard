0a45a3e33ad6f72ff37c2476b905771ccbd90249
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-08-23 14:39:15 +1200

    updating models

--- models/product.ts
+++ models/product.ts
@@ -1,6 +1,39 @@
 export interface Product {
   id: number
+  image_url: string
   name: string
   price: number
   description: string
+}
+
+export interface Category {
+  id: number
+  name: string
+  image_url: string
+}
+
+export interface User {
+  id: number
+  username: string
+  email: string
+  password: string
+}
+
+export interface Cart {
+  id: number
+  userId: number
+}
+
+export interface Order {
+  id: number
+  userId: number
+  orderStatus: string
+  orderTotal: number
+}
+
+export interface OrderItem {
+  id: number
+  orderId: number
+  productId: number
+  quantity: number
 }
--- server/db/migrations/20230823010129_products.js
+++
@@ -1,8 +0,0 @@
-exports.up = (knex) => {
-  return knex.schema.createTable('products', (table) => {
-    table.increments('id')
-    table.string('name')
-    table.price('number')
-    table.string('description')
-  })
-}
