2c5d443c65bdb840a0d41d12c2581f3e46ec75bf
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-08-28 11:30:33 +1200

    update seed files

--- server/db/seeds/05-snacks.js
+++ server/db/seeds/05-snacks.js
@@ -7,21 +7,21 @@ exports.seed = async function (knex) {
       name: 'Sweets Cookies',
       price: 1.99,
       description: 'Sugar free cookies for kids - Price is for each',
-      category_id: 4,
+      categoryId: 4,
     },
     {
       imageUrl: 'storage.com',
       name: 'Cheese Snacks',
       price: 2.99,
       description: 'Cheessy sticks - Price is for each',
-      category_id: 4,
+      categoryId: 4,
     },
     {
       imageUrl: 'storage.com',
       name: 'French Fries',
       price: 5.99,
       description: 'Tasty french fries - Price is for pack',
-      category_id: 4,
+      categoryId: 4,
     },
   ])
 }
--- server/db/seeds/06-drinks.js
+++ server/db/seeds/06-drinks.js
@@ -7,21 +7,21 @@ exports.seed = async function (knex) {
       name: 'Apple Juice',
       price: 3.99,
       description: 'Natural 1L apple juice - Price is for each',
-      category_id: 5,
+      categoryId: 5,
     },
     {
       imageUrl: 'storage.com',
       name: 'Watermelon Juice',
       price: 3.99,
       description: 'Natural 1L watermelon juice - Price is for each',
-      category_id: 5,
+      categoryId: 5,
     },
     {
       imageUrl: 'storage.com',
       name: 'Blueberry Juice',
       price: 2.99,
       description: 'Natural 1L blueberry juice - Price is for each',
-      category_id: 5,
+      categoryId: 5,
     },
   ])
 }
