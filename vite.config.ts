77de8fb96efc6855e0907d3be0223b83119c733a
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-08-31 15:58:07 +1200

    install homebrew and mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:3002' } },
+  server: {
+    https: {
+      key: 'localhost-key.pem',
+      cert: 'localhost.pem',
+    },
+  },
 })
