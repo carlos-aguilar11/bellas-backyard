c41cf5896bb2def72e3c6bf61c0a9c8e9c12ea57
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-01 13:53:14 +1200

    update Auth0 configuration

---
+++ auth0-cofig.ts
@@ -0,0 +1,8 @@
+// auth0-config.ts
+const auth0Config = {
+  domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  redirectUri: window.location.origin,
+}
+
+export default auth0Config
