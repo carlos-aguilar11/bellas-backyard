dc4aaaea1ecf41762e5a858ffd7a990065bb5f5b
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-09-11 16:08:21 +1200

    update knexfile and package.json

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -6,6 +6,7 @@ const path = require('path')
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: path.join(__dirname, 'dev.sqlite3'),
     },
