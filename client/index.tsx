bc23971d38eb7516deab16f190aa8651e45ed62a
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-10-12 15:02:51 +1300

    update Auth0 info access

--- client/index.tsx
+++ client/index.tsx
@@ -7,14 +7,23 @@ import App from './components//App/App'
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Auth0Provider
-      domain="carlos-aguilar.au.auth0.com"
-      clientId="j1ZK6s2kISVEuLr1bUzrlDtgMy8gzbwR"
-      redirectUri="http://localhost:3000/info"
-      audience="https://bellas-backyard/api"
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      domain={import.meta.env.VITE_AUTH0_DOMAIN as string}
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID as string}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE as string,
+        redirectUri: 'http://localhost:3000/info',
+      }}
     >
       <Router>
         <App />
       </Router>
-    </Auth0Provider>,
+    </Auth0Provider>
   )
 })
