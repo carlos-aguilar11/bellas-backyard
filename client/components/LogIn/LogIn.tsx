7a57b25a80c0962597cc57b2c446726dd1133499
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-10-18 13:05:44 +1300

    remove check user button and implement the functionality directly in the log in

--- client/components/LogIn/LogIn.tsx
+++ client/components/LogIn/LogIn.tsx
@@ -2,7 +2,7 @@ import { IfAuthenticated, IfNotAuthenticated } from '../Authenticated'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useNavigate } from 'react-router-dom'
 import { checkUserExists } from '../../apis/user'
-import { log } from 'console'
+import { useEffect, useCallback } from 'react' // Import useEffect and useCallback
 
 function LogIn() {
   const { user, logout, loginWithRedirect, getAccessTokenSilently } = useAuth0()
@@ -16,12 +16,13 @@ function LogIn() {
       console.error('Error during login:', error)
     }
   }
+
   const handleSignOut = () => {
     logout()
   }
 
   // This function checks if the user exists in your database
-  const checkUser = async () => {
+  const checkUser = useCallback(async () => {
     try {
       const token = await getAccessTokenSilently()
       const auth0Id = user?.sub // Get the Auth0 ID
@@ -30,25 +31,28 @@ function LogIn() {
         const userExists = await checkUserExists(token)
         if (userExists !== null) {
           // User exists in your database, redirect to the homepage
-          console.log('User exists, redirecting to homepage')
           navigate('/')
         } else {
           // User does not exist, redirect to the "info" page
-          console.log('User does not exist, redirecting to info')
-
           navigate('/info')
         }
       }
     } catch (error) {
       console.error('Error checking user:', error)
     }
-  }
+  }, [user, getAccessTokenSilently, navigate])
+
+  // Automatically check the user after a successful sign-in
+  useEffect(() => {
+    if (user) {
+      checkUser()
+    }
+  }, [user, checkUser])
 
   return (
     <>
       <h1>Welcome to Bella&apos;s Backyard</h1>
       <IfAuthenticated>
-        <button onClick={checkUser}>Check User</button>
         <button onClick={handleSignOut}>Sign out</button>
         {user && <p>Signed in as: {user.nickname}</p>}
       </IfAuthenticated>
