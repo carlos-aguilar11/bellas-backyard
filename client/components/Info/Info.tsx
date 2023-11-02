30442d41bda7c0584d59ef85bcb5a340c8069e03
carlos-aguilar11 <aguilar.pinero@gmail.com>
2023-11-02 15:49:04 +1300

    add form logic and user add route

--- client/components/Info/Info.tsx
+++ client/components/Info/Info.tsx
@@ -1,63 +1,104 @@
 import React, { useState } from 'react'
+import { User, useAuth0 } from '@auth0/auth0-react'
+import { NewUser } from '../../../models/user'
+import { createUser } from '../../apis/user'
+import { useNavigate } from 'react-router-dom'
 
 function Info() {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
   const [name, setName] = useState('')
   const [lastName, setLastName] = useState('')
   const [username, setUsername] = useState('')
   const [imageUrl, setImageUrl] = useState('')
-  const handleSubmit = (e: React.FormEvent) => {
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
+
+    if (isAuthenticated && user) {
+      const auth0Id = user?.sub || ''
+      const email = user?.email || ''
+
+      const defaultImage = `https://ui-avatars.com/api/?name=${name}+${lastName}&size=200`
+
+      const finalImage = imageUrl || defaultImage
+
+      const newUser: NewUser = {
+        auth0Id,
+        email,
+        name,
+        lastName,
+        username,
+        imageUrl: finalImage,
+      }
+      try {
+        const token = await getAccessTokenSilently()
+        const createdUser: User = await createUser(newUser, token)
+
+        console.log('User created:', createdUser)
+        navigate('/')
+      } catch (error) {
+        console.error('Error creating user:', error)
+      }
+    } else {
+      console.error('User is undefined')
+    }
   }
 
   return (
-    //create a form to add the user to our database
-    //take the auth0 id and email from the login then use the rest of the information to add the user
-
     <div>
       <h1>Complete your Profile</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={'name'}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            value={'lastName'}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={'username'}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="imageUrl">Image Link:</label>
-          <input
-            type="text"
-            id="imageUrl"
-            value={'imageUrl'}
-            onChange={(e) => setImageUrl(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Complete Profile</button>
-      </form>
+      {isAuthenticated ? (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Enter your Last Name"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter your Username"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="imageUrl">Image Link:</label>
+            <input
+              type="text"
+              id="imageUrl"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+              placeholder="Add a link to your picture"
+            />
+            <span>(optional)</span>
+          </div>
+          <button type="submit">Submit Profile</button>
+        </form>
+      ) : (
+        <div>Please sign in to complete your profile.</div>
+      )}
     </div>
   )
 }
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { validateAccessToken } from '../auth0'
 const router = express.Router()
 
-import { getUserByAuth0Id } from '../db/user'
+import { addUser, getUserByAuth0Id } from '../db/user'
 
 router.get('/', validateAccessToken, async (req, res) => {
   try {
@@ -19,6 +19,27 @@ router.get('/', validateAccessToken, async (req, res) => {
     }
 
     res.json({ profile })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.post('/', validateAccessToken, async (req, res) => {
+  try {
+    const auth0Id = req.auth?.payload.sub
+    if (!auth0Id) {
+      console.error('No auth0Id')
+      return res.status(401).send('Unauthorized')
+    }
+
+    const existingUser = await getUserByAuth0Id(auth0Id)
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+    const newUser = req.body
+    const createdUser = await addUser(newUser)
+    res.json({ profile: createdUser })
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: 'Something went wrong' })
